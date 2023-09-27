72d71f16d45662d89a24eb6663d535e7eab23bea
Marx Wang <marxw.me@gmail.com>
2023-09-27 20:40:16 +0800

    W03-P3: Display all menu from <Menu_46 />

--- demo/w03_46/src/App.js
+++ demo/w03_46/src/App.js
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import'./App.css';
 import menu_data from './data';
+import Menu_46 from './components/Menu_46';
 
 const App = () => {
   const [menu, setMenu] = useState(menu_data);
-  console.log(menu);
+  // console.log(menu);
   return (
     <section className="menu">
     <div className="title">
@@ -19,23 +20,8 @@ const App = () => {
       <button type="button" className="filter-btn" data-id="dessert">dessert</button>
       <button type="button" className="filter-btn" data-id="shakes">shakes</button>
     </div>
-    <div className="section-center">
-      {menu.map((item) => {
-        const { id, title, img, desc, price } = item;
-        return (
-          <article key={id} className="menu-item">
-            <img src={img} alt={title} className="photo" />
-            <div className="item-info">
-              <header>
-                <h4>{title}</h4>
-                <h4 className="price">{price}</h4>
-              </header>
-              <p className="item-text">{desc}</p>
-            </div>
-          </article>
-        );
-      })}
-    </div>
+
+    <Menu_46 menu={menu_data} />
   </section>
   );
 }
