c1fe02c62ab163176bb9b720361a09f30637e51e
Marx Wang <marxw.me@gmail.com>
2023-10-18 21:17:11 +0800

    W06-P5: Fetch data from node server via route /node_46, render all blogs data in webpage

--- demo/w06_blog_fullstack_46/client_blog_46/src/pages/BlogNodeServer_46.js
+++ demo/w06_blog_fullstack_46/client_blog_46/src/pages/BlogNodeServer_46.js
@@ -10,24 +10,50 @@ const BlogNodeServer_46 = () => {
       const response = await fetch(api_url);
       const json_data = await response.json();
       console.log('Blogs data', json_data);
-      return (json_data);
+      setBlogs(json_data);
     }
     catch (err) {
       console.log(err);
     }
   };
-
   useEffect(()=> {
-    setBlogs(fetchBlogFromNodeServer());
+    fetchBlogFromNodeServer();
   }, []);
-  console.log('Blogs', Blogs);
+  // console.log('Blogs', Blogs);
   return (
     <section className="blogs">
       <div className="section-title">
         <h2>latest blogs using breakpoints</h2>
         <h3>Marx Wang 211410146</h3>
       </div>
-      
+      <div className="blogs-center">
+      {
+        Blogs.map((blog) => {
+          const { id, title, img_remote, categories, content } = blog;
+          return (
+            <article
+              className="blog"
+              key={id}
+            >
+              <img
+                src={img_remote}
+                alt={title}
+                className="img blog-img"
+              />
+              <div className="blog-content">
+                <span>
+                  {categories}
+                  <i className="fa-solid fa-mug-saucer"></i>
+                </span>
+                <h3>{title}</h3>
+                <p>{content}</p>
+                <a href="#">read more</a>
+              </div>
+            </article>
+          )
+        })
+      }
+      </div>
     </section>
   )
 };
