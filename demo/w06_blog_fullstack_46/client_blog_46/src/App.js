d01afbefc253da427b1b544252c75ca56954389c
Marx Wang <marxw.me@gmail.com>
2023-10-18 19:20:38 +0800

    W06-P2: Blog theme html convert to React using scss.

--- demo/w06_blog_fullstack_46/client_blog_46/src/App.js
+++ demo/w06_blog_fullstack_46/client_blog_46/src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="blogs">
+      <div className="section-title">
+        <h2>latest blogs using breakpoints</h2>
+        <h3>Marx Wang 211410146</h3>
+      </div>
+      <div className="blogs-center">
+        <article className="blog">
+          <img
+            src="/images/photo-1.jpg"
+            alt="Coffee"
+            className="img blog-img"
+          />
+          <div className="blog-content">
+            <span>lifestyle <i className="fa-solid fa-mug-saucer"></i></span>
+            <h3>seven reasons why coffee is awesome</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            <a href="#">read more</a>
+          </div>
+        </article>
+        <article className="blog">
+          <img
+            src="/images/photo-2.jpg"
+            alt="Travel to Paris"
+            className="img blog-img"
+          />
+          <div className="blog-content">
+            <span>lifestyle <i className="fa-solid fa-globe"></i></span>
+            <h3>travel to paris</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            <a href="#">read more</a>
+          </div>
+        </article>
+        <article className="blog">
+          <img
+            src="/images/photo-3.jpg"
+            alt="Coffee"
+            className="img blog-img"
+          />
+          <div className="blog-content">
+            <span>lifestyle <i className="fa-solid fa-mug-saucer"></i></span>
+            <h3>seven reasons why coffee is awesome</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            <a href="#">read more</a>
+          </div>
+        </article>
+        <article className="blog">
+          <img
+            src="/images/photo-4.jpg"
+            alt="Coffee"
+            className="img blog-img"
+          />
+          <div className="blog-content">
+            <span>lifestyle <i className="fa-solid fa-mug-saucer"></i></span>
+            <h3>seven reasons why coffee is awesome</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            <a href="#">read more</a>
+          </div>
+        </article>
+      </div>
+    </section>
   );
 }
 
