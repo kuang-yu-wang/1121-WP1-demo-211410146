e26d2f22113ef536822319333817f69c2511da47
Marx Wang <marxw.me@gmail.com>
2023-10-25 20:07:48 +0800

    W07-P2: Fetch blog data from Supabase.

--- demo/w07_blog_fullstack_46/client_blog_46/src/App_46.js
+++ demo/w07_blog_fullstack_46/client_blog_46/src/App_46.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import BlogStaticPage_46 from './pages/BlogStaticPage_46';
 import BlogNodeServer_46 from './pages/BlogNodeServer_46';
 import HomePage_46 from './pages/HomePage_46';
+import BlogSupabase_46 from './pages/BlogSupabase_46';
 
 const App_46 = () => {
   return (
@@ -11,6 +12,7 @@ const App_46 = () => {
         <Route exact path='/' element={<HomePage_46 />} />
         <Route path='/static_46' element={<BlogStaticPage_46 />} />
         <Route path='/node_46' element={<BlogNodeServer_46 />} />
+        <Route path='/supa_46' element={<BlogSupabase_46 />} />
       </Routes>
     </BrowserRouter>
   );
