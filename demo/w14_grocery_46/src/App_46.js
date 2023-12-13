4825708f9d1a2e8d6e4128d5f5585cc6bfec7cff
marxw <211410146@o365.tku.edu.tw>
2023-12-13 19:44:56 +0800

    W14-P1: Store shopping list in local storage

---
+++ demo/w14_grocery_46/src/App_46.js
@@ -0,0 +1,97 @@
+import {useState, useEffect } from 'react';
+import Alert_46 from './Alert_46';
+import List_46 from './List_46';
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if (list){
+    return JSON.parse(localStorage.getItem(list));
+  }
+  return [];
+}
+
+const setLocalStorage = (items) => {
+  localStorage.setItem('list', JSON.stringify(items));
+}
+
+const defaultList = JSON.parse(localStorage.getItem('list') || []);
+
+const App_46 = () => {
+  const [name, setName] = useState('');
+  const [list, setList] = useState(defaultList);
+  const [alert, setAlert] = useState({
+    show:false,
+    msg:'', 
+    type:''
+  });
+
+  useEffect(()=> {
+    localStorage.setItem('list', JSON.stringify(list));
+  }, [list]);
+
+  const showAlert = (show=false, msg='', type='') => {
+    setAlert({show, msg, type});
+  }
+  
+  const clearList = () => {
+    showAlert(true, 'empty list', 'danger');
+    setList([]);
+    setLocalStorage([]);
+  };
+  const removeItem = (id) => {};
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name){
+      showAlert(true, 'please enter value', 'danger');
+    } else {
+      showAlert(true, 'item added to the list', 'success');
+      const newItem = {
+        id: new Date().getTime().toString(),
+        title: name
+      };
+      const newList = [...list, newItem];
+      setList(newList);
+      setLocalStorage(newList);
+      setName('');
+    }
+
+  }
+  return (
+    <>
+      <section className="section-center">
+        <form
+          className="grocery-form"
+          onSubmit={handleSubmit}
+        >
+          <h3>grocery bud -- Marx Wang</h3>
+          {alert.show && 
+            <Alert_46 {...alert} removeAlert={showAlert} />}
+          {/* form */}
+          <div className="form-control">
+            <input
+              type="text" 
+              className="grocery" 
+              placeholder="e.g. eggs" 
+              value={name} onChange={(e)=>setName(e.target.value)} 
+            />
+            <button type="submit" className="submit-btn">
+              submit
+            </button>
+          </div>
+          {/* end form */}
+        </form>
+        {/* list */}
+        {list.length > 0 &&
+          <div className='grocery-container'>
+            <List_46 items={list} removeItem={removeItem}/>
+            <button className="clear-btn" onClick={clearList}>
+              clear items
+            </button>
+          </div>
+        }
+      </section>
+    </>
+  );
+};
+export default App_46;
---
+++ demo/w14_grocery_46/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App_46 from './App_46';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App_46 />
+  </React.StrictMode>
+)
